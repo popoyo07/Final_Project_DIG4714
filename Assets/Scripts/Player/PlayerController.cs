bf4253da1b2983eb359013b6ce27d7d15773fff8
popoyo07 <154477545+popoyo07@users.noreply.github.com>
2025-03-11 14:50:35 -0400

    Player no longer constantly spinning

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -23,17 +23,23 @@ public class PlayerController : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * moveSpeed;
+        transform.rotation = Quaternion.LookRotation(movement);
 
         // this rotates the model to some way cause camera moves but player never rotates, but may need to hard code what is forward cause gets finnicky when moving right and left or moving camera around
         if (movement != Vector3.zero)
         {
             rb.MovePosition(transform.position + movement * moveSpeed * Time.fixedDeltaTime);
+            
         }
 
+
+/*
         if (horizontalInput != 0)
         {
             Quaternion targetRotation = Quaternion.Euler(0, horizontalInput * rotationSpeed * Time.fixedDeltaTime, 0);
             rb.MoveRotation(rb.rotation * targetRotation);
+     
         }
+*/
     }
 }
