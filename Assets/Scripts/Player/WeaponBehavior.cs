81330218491820bcfc41c03cc9661153c37757b0
popoyo07 <154477545+popoyo07@users.noreply.github.com>
2025-03-25 11:44:04 -0400

    added a new wepon and variables for future game logic

--- Assets/Scripts/Player/WeaponBehavior.cs
+++ Assets/Scripts/Player/WeaponBehavior.cs
@@ -7,22 +7,26 @@ public class WeaponBehavior : MonoBehaviour
 {
     public Collider c1;   // to later add collider info into it 
     bool canAttk;
-    VisualEffect bAttk;
+    VisualEffect vfxAttk;
     public GameObject player;
     Weapons w;
     public float theDMG;
     string attk;
+    public GameObject child;
+    public Vector3 rotationAxis = Vector3.up;
 
-    Coroutine attackCoroutine;
+
+    bool rotating;
+    public float rotationSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         attk = gameObject.name;
-        player = GameObject.Find("Santa");
+        player = GameObject.FindWithTag("Player");
         w = player.GetComponent<Weapons>();
        
-        bAttk = GetComponent<VisualEffect>();
+        vfxAttk = GetComponent<VisualEffect>();
         c1 = GetComponent<Collider>();
         canAttk = true; 
 
@@ -35,27 +39,42 @@ public class WeaponBehavior : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (rotating) 
+        {
+            transform.RotateAround(player.transform.position, rotationAxis, rotationSpeed * Time.deltaTime);
+        }
 
         if (canAttk)
         {
-            StartCoroutine(AttckRate(w.WeaponRateSec[attk]));
+            StartCoroutine(AttckRate(w.WeaponRateSec[attk], attk, w.WeaponAttDuration[attk]));
 
         }
-        theDMG = w.WeaponDMG[attk];
+        theDMG = w.WeaponDMG[attk]; // the keep up to date in case it increases
 
     }
-    IEnumerator AttckRate(float waitSeconds)
+    IEnumerator AttckRate(float waitSeconds, string attackName, float attackDuuration)
     {
-        canAttk = false;
-        c1.enabled = true;
+        switch (attackName)
+        {
+            case "BaseAttack":
+                Attacking();
+                break;
+            case "BookAttack":
+                Attacking();
+                rotating = true; // enable rotation
 
-        Debug.Log(" The Collider for the weapon is " + c1.enabled); 
-        bAttk.Play();
+                Debug.Log("rotating around player");
+                break;
+        }      
 
+        yield return new WaitForSeconds(attackDuuration); // how long the attack will last 
 
-        yield return new WaitForSeconds(0.3f); // how long the attack will last 
+        if (vfxAttk != null)
+        {
+            vfxAttk.Stop();
+        }
 
-        bAttk.Stop();
+        rotating = false; // desable rotation 
         c1.enabled = false;
 
         yield return new WaitForSeconds(waitSeconds); // attack cool down 
@@ -64,6 +83,19 @@ public class WeaponBehavior : MonoBehaviour
         Debug.Log(" The Collider for the weapon is " + c1.enabled);
 
 
+
+    }
+
+    void Attacking()
+    {
+        canAttk = false;
+        c1.enabled = true;
+        Debug.Log(" The Collider for the weapon is " + c1.enabled);
+        if (vfxAttk != null)
+        {
+            vfxAttk.Play();
+
+        }
 
     }
 }
--- Assets/Scripts/Player/Weapons.cs
+++ Assets/Scripts/Player/Weapons.cs
@@ -6,19 +6,22 @@ using UnityEngine.VFX;
 public class Weapons : MonoBehaviour
 {
     [Header(" BaseAttk ")]   
-    public float bAttackRateSec; // how often in seconds the attack is happening
-    public float bAttackDMG; // how much dmg
+    public float w1AttackRateSec; // how often in seconds the attack is happening
+    public float w1AttackDMG; // how much dmg
+    public float w1AttkDuration; // how long will the attack last
     
     [Header(" weapon 2 Attk")] // might reuse or not 
     public float w2AttackRateSec;
     public float w2AttackDMG;
+    public float w2AttkDuration;
     //GameObject weapon2;
-   // Collider c2;
+    // Collider c2;
 
 
     [Header(" weapon 3 Attk")]
     public float w3AttackRateSec;
     public float w3AttackDMG;
+    public float w3AttkDuration;
     // GameObject weapon3;
     //  Collider c3;
 
@@ -26,16 +29,23 @@ public class Weapons : MonoBehaviour
 
     public Dictionary<string, float> WeaponDMG = new Dictionary<string, float>();
     public Dictionary<string, float> WeaponRateSec = new Dictionary<string, float>();
+    public Dictionary<string, float> WeaponAttDuration = new Dictionary<string, float>();
     void Awake()
     {
-        WeaponDMG["BaseAttack"] = bAttackDMG;
-        WeaponDMG["placeNameHere2"] = w2AttackDMG;
+        //DMG
+        WeaponDMG["BaseAttack"] = w1AttackDMG;
+        WeaponDMG["BookAttack"] = w2AttackDMG;
         WeaponDMG["PlaceNameHere3"] = w3AttackDMG;
 
-
-        WeaponRateSec["BaseAttack"] = bAttackRateSec;
-        WeaponRateSec["placeNameHere2"] = w2AttackRateSec;
+        // Rate in seconds
+        WeaponRateSec["BaseAttack"] = w1AttackRateSec;
+        WeaponRateSec["BookAttack"] = w2AttackRateSec;
         WeaponRateSec["placeNameHere3"] = w3AttackRateSec;
+
+        // Attack duration
+        WeaponAttDuration["BaseAttack"] = w1AttkDuration;
+        WeaponAttDuration["BookAttack"] = w2AttkDuration;
+        WeaponAttDuration["placeNameHere3"] = w3AttkDuration;
 
         Debug.Log("Weapon Rate for BaseAttack: " + WeaponRateSec["BaseAttack"]);
     }
