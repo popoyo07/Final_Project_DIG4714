ba2718f6979273af180ac34b073e0dc6bf5a62df
popoyo07 <piwedaa@gmail.com>
2025-04-28 16:48:44 -0400

    fixing player animations

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -10,17 +10,25 @@ public class PlayerController : MonoBehaviour
     public float health;
 
     private Rigidbody rb;
-    Animator animator;
+    [SerializeField] Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        animator = GetComponent<Animator>();
+        if (gameObject.name == "Santa Claus")
+        {
+            animator = GetComponent<Animator>();
+        }
+        else
+        {
+             animator = GetComponentInChildren<Animator>();
+            Debug.Log("Getting the child");
+        }
     }
 
     void Update()
     {
-
+        
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
@@ -30,6 +38,7 @@ public class PlayerController : MonoBehaviour
         if (movement != Vector3.zero)
         {
             animator.SetBool("isMoving", true);
+            Debug.Log("The bool was updated and is moving should be true = " + animator.GetBool("isMoving"));
             rb.MovePosition(transform.position + movement * moveSpeed * Time.fixedDeltaTime);
             transform.rotation = Quaternion.LookRotation(movement);
 
