8a666745d3438f0afaa357ca8bbddda9c55f6651
Sze <sz982600@ucf.edu>
2025-04-28 03:16:55 -0400

    BUGGGGGY

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,9 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject CharacterSelection;
+    private CharacterUnlockUI characterUnlockUI;
+
     // 0 is santa 
     // 1 is Mrs Clause 
     // 2 is Rudolph
@@ -13,6 +16,16 @@ public class GameManager : MonoBehaviour
     //public bool[] playerChoice;
     // 
 
+    private void Start()
+    {
+        characterUnlockUI = CharacterSelection.GetComponent<CharacterUnlockUI>();
+    }
+
+    private void Update()
+    {
+        pChoice = CharacterUnlockUI.characterChosen;
+        Debug.LogWarning(pChoice);
+    }
 
 
 }
